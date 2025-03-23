208df65173ea24ac7887599b74f1af27c8aa07cd
Milk-Cool <coder.tomsk@gmail.com>
2025-03-23 14:46:53 +0300

    update script targets and make it clean up the output directory

--- build_and_copy.py
+++ build_and_copy.py
@@ -3,16 +3,29 @@ Import("env")
 import subprocess
 import shutil
 from os.path import join
+import os
 import sys
+
+PACKAGE_DIR = "frontend"
+IN_DIR = join("frontend", "dist")
+OUT_DIR = "data"
 
 def build_and_copy(source, target, env):
     p = subprocess.Popen(
         ["bash", "-i", "-c", "npm run build"] # Workaround for nvm
         if sys.platform == "linux" or sys.platform == "linux2"
         else ["npm", "run", "build"],
-        cwd="frontend")
+        cwd=PACKAGE_DIR)
     p.wait()
 
-    shutil.copytree(join("frontend", "dist"), "data", dirs_exist_ok=True)
+    shutil.rmtree(OUT_DIR)
 
-env.AddPreAction("upload", build_and_copy)
+    os.mkdir(OUT_DIR)
+    f = open(join(OUT_DIR, ".gitkeep"), "w")
+    f.close()
+
+    shutil.copytree(IN_DIR, OUT_DIR, dirs_exist_ok=True)
+
+env.AddPreAction("buildfs", build_and_copy)
+env.AddPreAction("uploadfs", build_and_copy)
+env.AddPreAction("uploadfsota", build_and_copy)
